295140bed0ca07ad957cef555597fd3ae2e96353
parv <parv0888@gmail.com>
2022-12-26 00:07:29 +0530

    register data dapp

---
+++ register-data-dapp/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./Header";
+import { useState } from "react";
+import { Container } from "@mui/material";
+import RegisterData from "./RegisterData";
+
+export default function App() {
+	const [isConnected, setConnected] = useState(false);
+
+	return (
+		<div className="App">
+			<Header
+				onConnected={() => setConnected(true)}
+				onDisconnected={() => setConnected(false)}
+			/>
+			<Container sx={{ mt: 5 }}>{isConnected && <RegisterData />}</Container>
+		</div>
+	);
+}
